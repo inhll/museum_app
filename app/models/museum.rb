a50249eceaaceac3e4c25e77e7fa2c24ef1ca106
Ian Hall <ian@flamingballofwreckage.net>
2011-01-21 12:55:26 -0600

    Updated colors, layout, etc

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -1,2 +1,15 @@
 class Museum < ActiveRecord::Base
+  attr_accessible :name, :website, :address, :city, :state, :zip_code, :free_days
+
+  validates :name,      :presence => true,
+                        :length => { :maximum => 80 }
+  validates :website,   :presence => true,
+                        :length => { :maximum => 80 }
+  validates :address,   :presence => true
+  validates :city,      :presence => true
+  validates :state,     :presence => true
+  validates :zip_code,  :presence => true,
+                        :length => { :is => 5 }
+  validates :free_days, :presence => true
+
 end
