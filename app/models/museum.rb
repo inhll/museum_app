1febfdf6c92be320cd80214681f56cd0e9d537dc
Ian Hall <ian@flamingballofwreckage.net>
2011-01-21 15:21:36 -0600

    Mucking about with the form errors

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -1,15 +1,17 @@
 class Museum < ActiveRecord::Base
   attr_accessible :name, :website, :address, :city, :state, :zip_code, :free_days
 
-  validates :name,      :presence => true,
-                        :length => { :maximum => 80 }
-  validates :website,   :presence => true,
-                        :length => { :maximum => 80 }
-  validates :address,   :presence => true
-  validates :city,      :presence => true
-  validates :state,     :presence => true
-  validates :zip_code,  :presence => true,
-                        :length => { :is => 5 }
+  validates_length_of :name,  
+                        :within => 5..80,
+                        :too_long => "should be shorter.",
+                        :too_short => "should be longer." 
+  validates_format_of :website, :with => /^(#{URI::regexp(%w(http https))})$/, :allow_blank => false
+  validates_length_of :address, :city,
+                        :within => 10..150,
+                        :too_long => "should be shorter.",
+                        :too_short => "should be longer."
+  validates :state,     :presence => true                          
+  validates :zip_code,  :presence => true
   validates :free_days, :presence => true
 
 end
